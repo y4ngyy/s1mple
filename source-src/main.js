5f87fb8d03316c11007b8644ab71e9918019ef68
y4ngyy <591620892@qq.com>
2019-10-24 20:38:51 +0800

    add aside layout

--- source-src/main.js
+++ source-src/main.js
@@ -4,32 +4,6 @@ import './scss/main.scss';
 
 hljs.initHighlightingOnLoad();
 
-function setBanner() {
-  let height = $(window).height();
-  let about = $('#about');
-  let top = height/2-about.height()/2;
-  $('#banner #about').css({
-    "top": top
-  }).css("display","block");
-}
-
-function moveAbout(scrollTop) {
-  let relativeHeight = $('#banner').height()/2+$('#about').height()/2;
-  let about = $('#about');
-  const right = 200;
-  if (right-scrollTop/relativeHeight*(right+about.width()) <= 0) {
-    about.fadeOut();
-    return;
-  } else {
-    about.fadeIn();
-  }
-  about.css({
-    "right": right - scrollTop / relativeHeight * (right + about.width()),
-    transition: "right 0.5s"
-  });
-
-}
-
 function scrollToTop() {
   let top = $('#navbar').position().top;
   window.scrollTo(0,top);
@@ -37,19 +11,15 @@ function scrollToTop() {
 }
 
 $(function () {
-    setBanner();
     $("pre code").each(function(){
         $(this).html("<ol><li>" + $(this).html().replace(/\n/g,"\n</li><li>") +"\n</li></ol>");
       });
 
     $(window).scroll(function() {
       let top = document.scrollingElement.scrollTop;
-      // let bannerHeight = $('#banner').height();
-      moveAbout(top);
       let navTop = $('#navbar').position().top;
       if (top > navTop) {
         if ($('.arrow').length<=0) {
-          console.log("insert");
           let i = $('<i class="arrow"></i>');
           i.click(scrollToTop);
           $('#container').after(i);
@@ -60,21 +30,13 @@ $(function () {
           $('.arrow').hide();
         }
       }
-      // if(top >= $('#navbar').offset().top) {
-      //   $('#navbar').css({
-      //     "position": "fixed",
-      //     "top": 0,
-      //     "transition": "position 0.5s"
-      //   });
-      // } else {
-      //   $('#navbar').removeAttr('style');
-      // }
     });
 
     $('.arrow-show').click(function () {
       let community = $('.community');
       if (community.is(':hidden')) {
         community.fadeIn();
+        community.css('display','inline-block');
         $('.arrow-show').css({
           "transform":"rotate(-90deg)",
           "transition":"transform 0.5s"
